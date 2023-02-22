a60a5a93448750492159b1146ac1ee2d9f7f62b3
LSP <sanchezlsp@gmail.com>
2023-02-22 10:48:26 +0100

    en updateQuality introduzco la separación del análisis si es Articulo o Item

---
+++ javascript/src/articulo.js
@@ -0,0 +1,59 @@
+const {Item} = require("./item") ;
+
+
+class Articulo extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality);
+  }
+  toString(){
+    //return return "nombre,sellIn,quality>>>>" + this.name + " " + this.sellIn + " " +  this.quality + " " +  this.type
+    console.log ('Neo, be aware of %o', this)
+    return `nombre>>${this.name} , sellIn>>${this.name} , quality>>>${this.name} `
+  }
+}
+
+class Articul extends Item {
+  static nuevo(name, sellIn, quality){
+    if (name === ''){
+      return new Articulo(name, sellIn, quality)
+    } else {
+      return new Item(name, sellIn, quality)
+    }
+  }
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality);
+  }
+  toString(){
+    //return return "nombre,sellIn,quality>>>>" + this.name + " " + this.sellIn + " " +  this.quality + " " +  this.type
+    console.log ('Neo, be aware of %o', this)
+    return `nombre>>${this.name} , sellIn>>${this.name} , quality>>>${this.name} `
+  }
+}
+
+
+class Estandar extends Articulo {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality);
+  }
+  set quality(valor){
+      if (valor === '') {
+        throw new Error(`name field of User cannot be empty`);
+      }
+      this.quality = valor;
+  }
+  get quality(){
+    return this.quality;
+  }
+  updateQuality(){
+    this.sellIn = this.sellIn - 1 ;
+    if (this.sellIn < 0){
+      this.setQuality(this.quality - 2)
+    }else {
+      this.setQuality(this.quality - 1)
+    }
+
+  }
+}
+
+
+  module.exports = {Articulo , Estandar}
